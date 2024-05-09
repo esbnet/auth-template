7eb8bf7910afd179f7cca1da15a1c41a39c3f77c
Edmilson Soares <esbnet@gmail.com>
2024-05-08 23:14:28 -0300

    feat: :sparkles: created Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+import { Poppins } from "next/font/google";
+import { PiLockKeyDuotone } from "react-icons/pi";
+
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
+import { cn } from "@/lib/utils";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function Home() {
-  useEffect(() => {
-    console.log("Hello");
-  }, []);
-  return <Button size="lg">Hello</Button>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-sky-400  to-blue-900">
+      <div className="space-y-6 text-center">
+        <h1
+          className={`${cn(
+            poppins.className
+          )} flex  flex-row justify-center text-6xl font-semibold text-white  drop-shadow-md text-center gap-2`}
+        >
+          <PiLockKeyDuotone className="inline" />
+          Auth
+        </h1>
+        <p className="text-lg text-white">Um simples serviço de autenticação</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size={"lg"}>
+              Entrar
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
